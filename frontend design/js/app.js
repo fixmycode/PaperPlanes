ae1d60836313c3531e17a82abb288eac085fa23c
mammut <irken.cs@gmail.com>
2014-04-07 11:54:09 -0300

    Cambiado el framework bootstrap por foundation 5. Ademas se termino la implementacion frontend usando AngularJS con resources

---
+++ frontend design/js/app.js
@@ -0,0 +1,28 @@
+var myApp = angular.module('app', ['ngResource']);
+
+myApp.controller('ContactsController', ['$scope', 'Contact', function($scope, Contact){
+    $scope.contacts = Contact.query();
+
+    $scope.submit = function(){
+        var newContact = new Contact({name: $scope.name, ip_address: $scope.ip_address, port: $scope.port});
+        newContact.$save(function(u, putResponseHeaders) {
+            $scope.name      = '';
+            $scope.ip_address = '';
+            $scope.port      = '';
+            if (typeof u.name === "string")
+                $scope.contacts.push(u);
+        });        
+    }
+
+    $scope.delete = function(contact) {
+        var index = $scope.contacts.indexOf(contact);
+        contact.$delete({id:contact.id}, function(){
+            $scope.contacts.splice(index,1);
+        });
+    }
+}]);
+
+
+myApp.factory('Contact', ['$resource', function($resource) {
+    return $resource('http://dev.chat.io/api/v1/contacts/:id');
+}]);
