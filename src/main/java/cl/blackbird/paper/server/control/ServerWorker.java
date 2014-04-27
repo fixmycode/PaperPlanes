824c7cd0150b0dff31cea9c2de150e68e49bacdb
Pablo Albornoz <pablo.albornoz.n@gmail.com>
2014-04-27 00:27:24 -0400

    ServerWorker pasa a ser parte del paquete de Control
    Como su objetivo es trabajar con el GUI entonces tiene más sentido que viva en su mismo paquete.

--- src/main/java/cl/blackbird/paper/server/control/ServerWorker.java
+++ src/main/java/cl/blackbird/paper/server/control/ServerWorker.java
@@ -1,11 +1,12 @@
-package cl.blackbird.paper.server;
+package cl.blackbird.paper.server.control;
 
-import cl.blackbird.paper.api.APIConfiguration;
+import cl.blackbird.paper.server.Server;
 
 import javax.swing.*;
 
 /**
- * ServerWorker allows the PaperServer to be run in the background of the GUI
+ * Le permite al servidor funcionar en el hilo de fondo de la interfaz, de esta manera no interrumpe la captura de
+ * evento de los controles y el servidor puede ser lanzado y detenido desde la GUI.
  */
 public class ServerWorker extends SwingWorker<Void, Boolean> {
     private Server server;
